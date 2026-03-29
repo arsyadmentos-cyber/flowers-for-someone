f2bf8494f9dab8a58857eadd75358681e126e83c
arsyadmentos-cyber <arsyadmentos@gmail.com>
2026-03-29 19:47:47 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `AISYAH JANGAN MERAJUK LAGI OKAY`.split('')
+const text = `HI AISYAHKUU`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
